b74da3bbb4c7b2605847ce33f5212312e6e6c322
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-04-12 13:25:36 +0000

    [Add] Admin Customers

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  #skip不要なルーティングを削除
+  #顧客用
+  devise_for :customers, skip: [:passwords], controllers:{
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  #管理者
+  devise_for :admin, skip: [:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
