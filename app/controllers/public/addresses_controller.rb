8f3141bd06765068d7b51cecbdd183ede72a6dc3
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-04-16 12:10:45 +0000

    [Add] address_form

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,23 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
+    @addresses = Address.all
+    @customer = current_customer
   end
-
+  
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to request.referer
+  end
+  
   def edite
   end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit( :customer_id, :name, :address)
+  end
+  
 end
